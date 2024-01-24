f45c97317c39888cd9085eb3225212b471ac9401
axel <axelselbach90@gmail.com>
2024-01-24 18:34:55 -0300

    feat: added header to app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { GithubLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
 import { useState } from 'react'
-import photo from './assets/fino_senhores.jpg'
+import { Header } from './components/Header/Header'
 
 import './index.css'
 
@@ -18,24 +19,14 @@ export function App() {
       >
         <div className="backdrop-blur-sm mt-0 m-auto max-w-5xl w-full flex h-screen">
           <main className="dark:text-white text-primary-100 w-full h-full mx-3 flex flex-col justify-center items-center gap-9 z-10 p-7">
-            <header className="flex flex-col items-center gap-9">
-              <div className="border-2 border-primary-500 dark:border-sky-600 transition-colors duration-200 h-32 w-32 sm:h-40 sm:w-40 rounded-full p-1">
-                <img
-                  className="rounded-full w-full"
-                  src={photo}
-                  alt="Foto do meliante que escreveu este código"
-                />
-              </div>
-
-              <label className="relative inline-flex items-center cursor-pointer">
-                <input className="sr-only peer" value="" type="checkbox" />
-                <div
-                  onClick={toggleDarkMode}
-                  className="w-24 h-12 rounded-full ring-0 peer duration-500 outline-none bg-gray-200 overflow-hidden before:flex before:items-center before:justify-center after:flex after:items-center after:justify-center before:content-['☀️'] before:absolute before:h-10 before:w-10 before:top-1/2 before:bg-white before:rounded-full before:left-1 before:-translate-y-1/2 before:transition-all before:duration-300 peer-checked:before:opacity-0 peer-checked:before:rotate-90 peer-checked:before:-translate-y-full shadow-lg shadow-gray-400 peer-checked:shadow-lg peer-checked:shadow-gray-700 peer-checked:bg-primary-100 after:content-['🌑'] after:absolute after:bg-[#1d1d1d] after:rounded-full after:top-[4px] after:right-1 after:translate-y-full after:w-10 after:h-10 after:opacity-0 after:transition-all after:duration-300 peer-checked:after:opacity-100 peer-checked:after:rotate-180 peer-checked:after:translate-y-0"
-                ></div>
-              </label>
-            </header>
-
+            <Header />
+            <label className="relative inline-flex items-center cursor-pointer">
+              <input className="sr-only peer" value="" type="checkbox" />
+              <div
+                onClick={toggleDarkMode}
+                className="w-24 h-12 rounded-full ring-0 peer duration-500 outline-none bg-gray-200 overflow-hidden before:flex before:items-center before:justify-center after:flex after:items-center after:justify-center before:content-['☀️'] before:absolute before:h-10 before:w-10 before:top-1/2 before:bg-white before:rounded-full before:left-1 before:-translate-y-1/2 before:transition-all before:duration-300 peer-checked:before:opacity-0 peer-checked:before:rotate-90 peer-checked:before:-translate-y-full shadow-lg shadow-gray-400 peer-checked:shadow-lg peer-checked:shadow-gray-700 peer-checked:bg-primary-100 after:content-['🌑'] after:absolute after:bg-[#1d1d1d] after:rounded-full after:top-[4px] after:right-1 after:translate-y-full after:w-10 after:h-10 after:opacity-0 after:transition-all after:duration-300 peer-checked:after:opacity-100 peer-checked:after:rotate-180 peer-checked:after:translate-y-0"
+              ></div>
+            </label>
             <ul className="flex flex-col flex-wrap w-full gap-10">
               <li className="w-full h-20 flex flex-wrap items-center justify-center">
                 <a
