662084faafe842fde151565e56fa476f5f269963
axel <axelselbach90@gmail.com>
2024-01-24 18:27:33 -0300

    feat: created header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
-import { Button } from '../Button/Button'
-import photo from './assets/fino_senhores.jpg'
+import photo from '../../assets/fino_senhores.jpg'
 
 export function Header() {
-  const [darkMode, setDarkMode] = useState(false)
-
   return (
     <header className="flex flex-col items-center gap-9">
       <div className="border-2 border-primary-500 dark:border-sky-600 transition-colors duration-200 h-32 w-32 sm:h-40 sm:w-40 rounded-full p-1">
@@ -13,7 +9,6 @@ export function Header() {
           src={photo}
           alt="Foto do meliante que escreveu este código"
         />
-        <Button darkMode={darkMode} setDarkMode={setDarkMode} />
       </div>
     </header>
   )
