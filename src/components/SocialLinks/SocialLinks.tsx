eaddb406547117aa0c94ffb76ab709b9c266fd58
axel <axelselbach90@gmail.com>
2024-01-25 16:22:36 -0300

    fix: added animation and new header width

--- src/components/SocialLinks/SocialLinks.tsx
+++ src/components/SocialLinks/SocialLinks.tsx
@@ -1,12 +1,12 @@
-export interface SocialLinksProps {
+export interface SocialLinkProps {
+  href: string
   icon: React.ReactNode
   text: string
-  href: string
 }
 
-export function SocialLinks({ icon, text, href }: SocialLinksProps) {
+export function SocialLink({ href, icon, text }: SocialLinkProps) {
   return (
-    <li className="w-full h-20 flex flex-wrap items-center justify-center">
+    <li className="w-full h-20 flex flex-wrap items-center justify-center animate-leftRigth ">
       <a
         className="flex items-center justify-center gap-2 h-auto w-80 sm:w-full p-4 dark:bg-primary-100 bg-white border-solid border-2 border-primary-500 dark:border-sky-600 rounded-2xl hover:shadow-none shadow-wine-shadow dark:shadow-blue-shadow dark:hover:shadow-none hover:translate-y-0 -translate-y-1 transition ease-in duration-175"
         href={href}
