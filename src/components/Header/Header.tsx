cced657ffb3ffea11ed062d16c53e0c68984d70e
axel <axelselbach90@gmail.com>
2024-01-25 16:43:00 -0300

    feat: finished header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,8 +2,8 @@ import photo from '../../assets/fino_senhores.jpg'
 
 export function Header() {
   return (
-    <header className="flex flex-col items-center gap-9">
-      <div className="border-2 border-primary-500 dark:border-sky-600 transition-colors duration-200 h-32 w-32 sm:h-40 sm:w-40 rounded-full p-1">
+    <header className="flex flex-col items-center gap-9 animate-topDown">
+      <div className="border-2 border-primary-500 dark:border-sky-600 transition-colors duration-200 h-36 w-36 sm:h-40 sm:w-40 rounded-full p-1">
         <img
           className="rounded-full w-full"
           src={photo}
